224419f79e7ea042cce1fc1926c0ea26595f8e2e
Ruslan Simeonov <ruslansimeonov@gmail.com>
2024-11-18 16:15:33 +0100

    First Leet Code Solution

---
+++ src/leet_code_questions/mergeTwoSortedLists.ts
@@ -0,0 +1,49 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let dummy = new ListNode();
+    let current = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    if (list1 !== null) {
+        current.next = list1;
+    } else {
+        current.next = list2;
+    }
+
+    return dummy.next;
+}
+
+// Example usage:
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const mergedList = mergeTwoLists(list1, list2);
+
+function printList(head: ListNode | null): void {
+    let current = head;
+    while (current !== null) {
+        console.log(current.val);
+        current = current.next;
+    }
+}
+
+printList(mergedList);
